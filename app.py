2914593393bcfabccbc5b64731934b23c3e29d14
miguelsancor <miguelsancor_2005@hotmail.com>
2025-02-21 22:42:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, send_file
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=".")
 
 @app.route("/")
 def home():
-    return send_file("index.html")
+    return send_from_directory(".", "index.html")  # Sirve el index.html desde el mismo directorio
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Usar el puerto definido en Coolify
+    app.run(host="0.0.0.0", port=port)
